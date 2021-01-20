e3cbffd56f0501e08443463003f7ae3ba52e60ab
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2021-01-20 15:39:39 +0100

    update: first commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Audio} from 'expo-av'
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [sound, setSound] = useState()
+
+  async function handleChocMental() {
+    const {sound} = await Audio.Sound.createAsync(
+      require('./assets/choc_mental.mp3')
+    )
+    setSound(sound)
+
+    await sound.playAsync()
+  }
+
+  useEffect(() => {
+    return sound ? () => {
+      sound.unloadAsync()
+    } : undefined;
+  }, [sound])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Un Bon Moment</Text>
+      <Button title={"Choc Mental !"} onPress={() => handleChocMental()}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +33,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    marginTop: 50,
+    fontSize: 30,
+    textTransform: 'uppercase'
+  },
+  buttonStyle: {
+    flex: 3,
+    borderWidth: 1,
+    borderColor: 'black'
+  }
 });
